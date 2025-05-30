2f1f41538dc58bc907e6b4fbbb70aafcd6556c1b
Solobored <josneiba10@gmail.com>
2025-05-30 00:11:10 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
-import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
   root: "src/",
@@ -9,12 +8,12 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(fileURLToPath(new URL("./src/index.html", import.meta.url))),
-        cart: resolve(fileURLToPath(new URL("./src/cart/index.html", import.meta.url))),
-        checkout: resolve(fileURLToPath(new URL("./src/checkout/index.html", import.meta.url))),
-        checkouttwo: resolve(fileURLToPath(new URL("./src/checkout/success.html", import.meta.url))),
-        product: resolve(fileURLToPath(new URL("./src/product_pages/index.html", import.meta.url))),
-        productlisting: resolve(fileURLToPath(new URL("./src/product_listing/index.html", import.meta.url))),
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkouttwo: resolve(__dirname, "src/checkout/success.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        productlisting: resolve(__dirname, "src/product_listing/index.html"),
       },
     },
   },
